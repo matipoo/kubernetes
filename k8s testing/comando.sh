8b4a9f3bd36b6d393b8de8393d3eceea3044945a
unknown <matiaspozzi@gmail.com>
2021-06-17 21:07:50 -0300

    namepsaces-services-rc

--- k8s testing/comando.sh
+++ k8s testing/comando.sh
@@ -2,4 +2,12 @@ kubectl apply -f 00-namespaces.yaml #crea el namespace
 
 kubectl get ns #muestra los namespaces. Debes ver uno que se llama testing
 
+kubectl -n testing apply -f 01-wordpress-services.yaml # aplico el servicio configurado sobre el namespace testing
 
+kubectl -n testing get svc #reviso los servicios aplicados en el namespace testing
+
+kubectl -n testing apply -f 02-wordpress-rc.yaml #aplico el pod
+
+kubectl -n testing get pods #reviso que el pod corra
+
+kubectl get nodes -o wide #reviso la ip publica
